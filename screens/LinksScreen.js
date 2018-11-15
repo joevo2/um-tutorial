11da4af15ed5fea1b9d8baab925c546744b09e00
Joel <joevo2@gmail.com>
2018-11-15 14:05:14 +0800

    added add amount screen

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,27 +1,81 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+ import { ScrollView, StyleSheet, Text, Platform, View } from 'react-native';
+ import { TextInput } from 'react-native-gesture-handler';
 
-export default class LinksScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Links',
-  };
+ export default class LinksScreen extends React.Component {
+   static navigationOptions = {
+     title: 'Add Item',
+   };
 
-  render() {
-    return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
-    );
-  }
-}
+   state = { amount: '' }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
-});
+   render() {
+     return (
+       <View style={styles.container}>
+         <ScrollView>
+           <View style={styles.row}>
+             <Text style={{ fontSize: 18 }}>Price</Text>
+             <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+               <Text>RM </Text>
+               <TextInput
+                 style={styles.textInput}
+                 onChangeText={(input) => this.setState({ amount: input })}
+                 value={this.state.text}
+                 keyboardType='numeric'
+               />
+             </View>
+           </View>
+         </ScrollView>
+         <View style={styles.tabBarStickyBottom}>
+           <Text style={{ fontWeight: 'bold' }}>Total</Text>
+           <Text style={{ fontWeight: 'bold' }}>RM 10</Text>
+         </View>
+       </View>
+     );
+   }
+ }
+
+ const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     backgroundColor: '#fff',
+   },
+   row: {
+     flexDirection: 'row',
+     justifyContent: 'space-between',
+     alignItems: 'center',
+     paddingHorizontal: 10,
+     paddingVertical: 30,
+     borderWidth: 0.5,
+     borderColor: '#d6d7da',
+   },
+   textInput: {
+     width: 80,
+     borderWidth: 0.5,
+     borderColor: '#d6d7da',
+     padding: 10
+   },
+   tabBarStickyBottom: {
+     position: 'absolute',
+     bottom: 0,
+     left: 0,
+     right: 0,
+     ...Platform.select({
+       ios: {
+         shadowColor: 'black',
+         shadowOffset: { height: -3 },
+         shadowOpacity: 0.1,
+         shadowRadius: 3,
+       },
+       android: {
+         elevation: 20,
+       },
+     }),
+     alignItems: 'center',
+     backgroundColor: '#fbfbfb',
+     paddingVertical: 20,
+     flexDirection: 'row',
+     justifyContent: 'space-between',
+     padding: 10,
+   }
+ });
