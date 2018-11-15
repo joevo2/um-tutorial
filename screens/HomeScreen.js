f892865fb9de52f63204e8516d4f1a58ba20242a
Joel <joevo2@gmail.com>
2018-11-15 13:52:00 +0800

    added bottom static bar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,7 +3,8 @@ import {
   View,
   StyleSheet,
   ScrollView,
-  Text
+  Text,
+  Platform
 } from 'react-native';
 
 export default class HomeScreen extends React.Component {
@@ -17,6 +18,10 @@ export default class HomeScreen extends React.Component {
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
           <Text>Test World</Text>
         </ScrollView>
+        <View style={styles.tabBarInfoContainer}>
+           <Text style={{fontWeight: 'bold'}}>Total</Text>
+           <Text style={{fontWeight: 'bold'}}>RM 10</Text>
+         </View>
       </View>
     );
   }
@@ -30,4 +35,27 @@ const styles = StyleSheet.create({
   contentContainer: {
     paddingTop: 30,
   },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+  }
 });
